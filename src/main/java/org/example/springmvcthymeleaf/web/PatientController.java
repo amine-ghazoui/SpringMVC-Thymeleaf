a446f3c199de72ff4522026a848daf6deec3b249
amine-ghazoui <ghazouimohamedamine@gmail.com>
2025-03-17 15:58:11 +0100

    Add templates creation

--- src/main/java/org/example/springmvcthymeleaf/web/PatientController.java
+++ src/main/java/org/example/springmvcthymeleaf/web/PatientController.java
@@ -1,13 +1,18 @@
 package org.example.springmvcthymeleaf.web;
 
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.example.springmvcthymeleaf.entities.Patient;
 import org.example.springmvcthymeleaf.repositories.PatientRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -19,9 +24,37 @@ public class PatientController {
 
 
     @GetMapping("/index")
-    public String index(Model model) {
-        List<Patient> patients = patientRepository.findAll();
-        model.addAttribute("listPatients", patients);
+    public String index(Model model,
+                        @RequestParam(name = "page", defaultValue = "0") int page,
+                        @RequestParam(name = "size", defaultValue = "5") int size,
+                        @RequestParam(name = "keyword", defaultValue = "")String keyword) {
+        Page<Patient> pagePatients = patientRepository.findByNomContains(keyword, PageRequest.of(page, size));
+        model.addAttribute("ListePatients", pagePatients.getContent());
+        model.addAttribute("pages", new int[pagePatients.getTotalPages()]);
+        model.addAttribute("pageCurrent", page);
+        model.addAttribute("keyword", keyword);
         return "patients";
+    }
+
+    @GetMapping("/delete")
+    public String delete(Long id, int page, int size) {
+
+        patientRepository.deleteById(id);
+        return "redirect:/index?page=" + page + "&size=" + size;
+    }
+
+    @GetMapping("formPatient")
+    public String formPatient(Model model) {
+
+        model.addAttribute("patient", new Patient());
+        return "patientForm";
+    }
+
+    public String save (Model model,@Valid Patient patient,
+                        @RequestParam(defaultValue = "0") int page,
+                        @RequestParam(defaultValue = "") String keyword) {
+
+        patientRepository.save(patient);
+        return "redirect:/index?page="+page+"&keyword="+keyword;
     }
 }
