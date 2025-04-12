f297a63ba12c59b2ed6f6d92bb46df355e1e2e4c
amine-ghazoui <ghazouimohamedamine@gmail.com>
2025-04-12 13:25:43 +0100

    JDBC Authentication

--- src/main/java/org/example/springmvcthymeleaf/SpringMvcThymeleafApplication.java
+++ src/main/java/org/example/springmvcthymeleaf/SpringMvcThymeleafApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 import java.util.Date;
 
@@ -19,7 +21,7 @@ public class SpringMvcThymeleafApplication {
         SpringApplication.run(SpringMvcThymeleafApplication.class, args);
     }
 
-    //@Bean
+    @Bean
     CommandLineRunner commandLineRunner(PatientRepository patientRepository) {
         return args -> {
 
@@ -34,7 +36,24 @@ public class SpringMvcThymeleafApplication {
         };
     }
 
+    @Bean
+    CommandLineRunner commandLineRunner(JdbcUserDetailsManager jdbcUserDetailsManager) {
 
+        PasswordEncoder passwordEncoder = passwordEncoder();
+        return args -> {
+            jdbcUserDetailsManager.createUser(
+                    User.withUsername("user11").password(passwordEncoder.encode("1234")).roles("USER").build()
+            );
+
+            jdbcUserDetailsManager.createUser(
+                    User.withUsername("user22").password(passwordEncoder.encode("1234")).roles("USER").build()
+            );
+
+            jdbcUserDetailsManager.createUser(
+                    User.withUsername("admin2").password(passwordEncoder.encode("1234")).roles("USER", "ADMIN").build()
+            );
+        };
+    }
     @Bean
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
--- src/main/java/org/example/springmvcthymeleaf/sec/SecurityConfig.java
+++ src/main/java/org/example/springmvcthymeleaf/sec/SecurityConfig.java
@@ -1,20 +1,36 @@
 package org.example.springmvcthymeleaf.sec;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import javax.sql.DataSource;
+
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Bean
+    public JdbcUserDetailsManager jdbcUserDetailsManager(DataSource dataSource) {
+        //(Spécifier le data source)dans quelle DB ou se trouve les utilisateurs et les roles
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    //@Bean
     public InMemoryUserDetailsManager inMemoryUserDetailsManager(PasswordEncoder passwordEncoder) {
         String encodedPassword = passwordEncoder.encode("1234");
         return new InMemoryUserDetailsManager(
